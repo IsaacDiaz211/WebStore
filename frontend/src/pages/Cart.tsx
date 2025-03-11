e4c3e065dce520cddbf41a221369f465794c1825
Isaac <isaac211.md@outlook.com>
2025-03-11 18:52:34 -0300

    register page

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,9 +1,11 @@
 import GeneralTable from "../components/GeneralTable";
+import Button from "react-bootstrap/Button";
 const Cart = () => {
     return (
         <>
             <h2>Bienvenido a Carrito</h2>
             <GeneralTable/>
+            <Button variant="primary">Comprar</Button>
         </>
     );
 };
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,5 +1,13 @@
+import AddProfile from "../components/AddProfile";
+
 const SignIn = () => {
-    return <h1>Registrarse</h1>;
+    return (
+        <>
+            <h1>Registrarse</h1>
+            <AddProfile />
+        </>
+    );
+    
 };
 
 export default SignIn;
